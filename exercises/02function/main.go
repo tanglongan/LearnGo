d5b42c42eaa076744114cdb67d917a10582d3bb4
tanglongan <ishtangla303@163.com>
2021-06-19 15:43:49 +0800

    update code

---
+++ exercises/02function/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	f := double()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+
+}
+
+func double() func() int {
+	var r int
+	return func() int {
+		r++
+		return r * 2
+	}
+}
